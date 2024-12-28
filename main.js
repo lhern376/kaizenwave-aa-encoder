cbf85a895dd05f424164eca5d0c5aed9d796c29b
leduan001 <lhern376@fiu.edu>
2024-12-28 16:02:18 -0500

    adds three.js text

--- main.js
+++ main.js
@@ -20,6 +20,8 @@
 
 //  * ---- Background Parallax
 
+//  * ---- NEUMORPHISM TYPO
+
 /* ------------------------------------------------------------------------------------------------------------------------------------------ */
 
 /**
@@ -322,6 +324,36 @@ hey_there_other_arr.forEach((entry) => {
   hey_there_observer_other.observe(entry);
 });
 
+// --------------- three.js canvas
+
+const three_js_canvas = document.querySelector("#canvas-wrapper");
+
+const hey_there_observer_canvas = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      three_js_canvas.classList.toggle(
+        "cmstl-canvas-transition",
+        entry.isIntersecting
+      );
+
+      if (entry.isIntersecting) {
+        hey_there_other_arr.forEach((elem) => {
+          elem.classList.add("cmstl-reduce-opacity");
+        });
+      } else {
+        hey_there_other_arr.forEach((elem) => {
+          elem.classList.remove("cmstl-reduce-opacity");
+        });
+      }
+    });
+  },
+  { rootMargin: "-50px" }
+);
+
+const three_js_section = document.querySelector("#cmid-what-we-do");
+
+hey_there_observer_canvas.observe(three_js_section);
+
 // ---------------------------------------------------- What we do section text
 
 // --------------- xl text
@@ -603,3 +635,167 @@ function setParallaxScrollProp() {
 }
 
 setParallaxScrollProp(); // run the function in case page is reloaded
+
+/*
+ * ---- NEUMORPHISM TYPO
+ *
+ * Made with ThreeJS - Enjoy!
+ *
+ * Experimenting with neumorphism in typography.
+ * Use cursor to move around the shiny effect.
+ * On mobile touch + drag screen.
+ *
+ * #034 - #100DaysOfCode
+ * By ilithya | 2020
+ * https://www.ilithya.rocks/
+ * https://twitter.com/ilithya_rocks
+ */
+
+// const colorBg = "hotpink"; // #ff69b4
+const colorBg = "rgba(13,13,13,0)";
+// const colorTypo = "#ff49a4"; // dark pink
+// const colorTypo = "rgb(6,54,134)";
+// const colorTypo = "rgb(123, 201, 239)";
+// const colorTypo = "rgb(23,67,154)";
+const colorTypo = "rgb(93, 155, 219)";
+
+const nearDist = 0.1;
+// const nearDist = 100;
+const farDist = 10000;
+// const farDist = 1000;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  nearDist,
+  farDist
+);
+// camera.position.z = Math.round(farDist / 20);
+camera.position.z = Math.round(farDist / 15);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+// responsive size (CUSTOM)
+const content_max_width = 1530;
+window.addEventListener("resize", setRendererSize);
+function setRendererSize() {
+  const width = Math.min(window.innerWidth / 2, content_max_width / 2);
+  const height = window.innerHeight / 2;
+
+  // const mql = window.matchMedia("(max-width: 767px)");
+  // if (mql.matches) renderer.setSize(width * 2, height);
+  // else
+  renderer.setSize(width * 2, height);
+}
+setRendererSize();
+// (CUSTOM)
+
+renderer.setClearColor(colorBg);
+renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+document.querySelector("#canvas-wrapper").appendChild(renderer.domElement);
+
+const light = new THREE.DirectionalLight(0xffdffd, 1);
+light.position.set(-15, 0, 70);
+scene.add(light);
+
+// CREATE TYPOGRAPHY
+const group = new THREE.Group();
+const typoLoader = new THREE.FontLoader();
+const createTypo = (font) => {
+  const word = `We're
+Kaizen
+Wave`;
+  const typoSize = 120;
+  // const typoHeight = Math.round(typoSize / 4);
+  const typoHeight = Math.round(typoSize / 6);
+  const typoProperties = {
+    font: font,
+    size: typoSize,
+    // height: 0,
+    height: typoHeight,
+    curveSegments: 20,
+    bevelEnabled: false,
+    bevelThickness: 0.1,
+    bevelSize: 0.5,
+    bevelOffset: 0,
+    bevelSegments: 10,
+  };
+  const textMesh = new THREE.Mesh();
+  textMesh.geometry = new THREE.TextBufferGeometry(word, typoProperties);
+  textMesh.material = new THREE.MeshStandardMaterial({
+    color: colorBg,
+    emissive: colorTypo,
+    roughness: 0,
+    metalness: 1,
+    // side: THREE.DoubleSide,
+    transparent: true,
+    // opacity: 0.7,
+    opacity: 1,
+  });
+
+  textMesh.geometry.computeBoundingBox();
+  textMesh.geometry.boundingBox.getCenter(textMesh.position).multiplyScalar(-1);
+
+  textMesh.matrixAutoUpdate = false;
+  textMesh.updateMatrix();
+
+  group.add(textMesh);
+};
+typoLoader.load(
+  "https://threejs.org/examples/fonts/helvetiker_bold.typeface.json",
+  createTypo
+);
+scene.add(group);
+
+// CREATE PART OF THE MOUSE/TOUCH OVER EFFECT
+let mouseX = 0;
+let mouseY = 0;
+const mouseFX = {
+  windowX: Math.round(window.innerWidth / 2),
+  windowY: Math.round(window.innerHeight / 2),
+  coordinates: function (cX, cY) {
+    mouseX = cX - this.windowX;
+    mouseY = cY - this.windowY;
+  },
+  onMouseMove: function (e) {
+    mouseFX.coordinates(e.clientX, e.clientY);
+  },
+  onTouchMove: function (e) {
+    const touchX = e.changedTouches[0].clientX;
+    const touchY = e.changedTouches[0].clientY;
+    mouseFX.coordinates(touchX, touchY);
+  },
+};
+
+// update windowX and windowY when resizing (CUSTOM)
+window.addEventListener("resize", setMouseFX);
+function setMouseFX() {
+  mouseFX.windowX = Math.round(window.innerWidth / 2);
+  mouseFX.windowY = Math.round(window.innerHeight / 2);
+}
+// (CUSTOM)
+
+document.addEventListener("mousemove", mouseFX.onMouseMove);
+document.addEventListener("touchmove", mouseFX.onTouchMove);
+
+// RENDERING
+const render = () => {
+  const ct = 0.05;
+  // const ct = 0.01;
+  // const ct = 0.5;
+  camera.position.x += (mouseX - camera.position.x) * ct;
+  camera.position.y += (mouseY - camera.position.y) * ct;
+  camera.lookAt(scene.position);
+
+  const r = Date.now() * 0.0018;
+  const rot = Math.sin(r) * 0.12;
+  group.rotation.x = rot * 1.4;
+  group.rotation.y = rot;
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+};
+render();
